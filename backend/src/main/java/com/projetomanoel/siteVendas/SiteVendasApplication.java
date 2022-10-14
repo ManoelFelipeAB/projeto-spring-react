f14b1bc83e3b856549246c985644daea78112705
ManoelFelipeAB <manoelfelipe.s33@gmail.com>
2022-10-14 13:17:19 -0300

    Project created

---
+++ backend/src/main/java/com/projetomanoel/siteVendas/SiteVendasApplication.java
@@ -0,0 +1,13 @@
+package com.projetomanoel.siteVendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiteVendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/projetomanoel/siteVendas/SiteVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetomanoel.siteVendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiteVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
